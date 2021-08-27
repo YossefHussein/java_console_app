eeedb1033ea176adb7aa602379a63e19defff82e
Youssef Hussein <youssef.hussein.businss@gmail.com>
2021-08-27 15:56:22 +0200

    upgrade

--- src/com/company/String.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-public class String {
-    public static void main(String[] args) {
-     String MyName = "youssef";
-     String MyName2 = "hussein";
-     System.out.println(MyName + " " + MyName2);
-    }
-}
---
+++ src/com/company/textInJava.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class textInJava {
+    public static void main(String[] args) {
+      String MyName = "youssef";
+      String MyName2 = "hussein";
+System.out.println(MyName+ MyName2);
+    }
+}
